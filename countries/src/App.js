d4631413baa83913cbdeef9d62e3fd714d14095f
Xzrm <maciekkra@gmail.com>
2020-06-09 22:08:57 +0200

    improved syntax

--- countries/src/App.js
+++ countries/src/App.js
@@ -3,78 +3,64 @@ import axios from 'axios'
 import Country from './Components/Country'
 
 
-
-const filterObj = (listObj, query) => {
-  const foundObj = []
-  for (var i = 0; i < listObj.length; i++) {
-    if ((listObj[i].name.toLowerCase().includes(query))
-      && (query !== "")) {
-      foundObj.push(listObj[i])
-    }
-  }
-  console.log(foundObj)
-  return (foundObj)
-}
-
 const App = (props) => {
 
   const [countries, setCountries] = useState([])
   const [query, setQuery] = useState('')
   const [foundCountries, setFoundCountries] = useState([])
 
-  const showCountry = (country) => {
+
+  const showSingleCountry = (country) => {
     setFoundCountries([country])
-    displayCountry()
   }
 
 
-  const displayCountry = () => {
-    foundCountries.map(obj => console.log(obj.name))
-    if (foundCountries.length > 10) {
-      return ('Too many matches, specify another filter')
-    } else if (foundCountries.length === 1) {
-      return <Country country={foundCountries[0]} />
-    } else {
-      return (
-        foundCountries.map(country =>
-          <div key={country.name}> {country.name}
-            <button onClick={() => showCountry(country)}>show</button></div>
-        ))
-    }
-  }
-
-
-  const handleFilter = (event) => {
+  const handleQuery = (event) => {
     setQuery(event.target.value)
-    setFoundCountries(filterObj(countries, event.target.value))
-    console.log('------------------------------------')
   }
 
   useEffect(() => {
-    console.log('effect')
-
-    const eventHandler = response => {
-      console.log('promise fulfilled')
-      setCountries(response.data)
-    }
-
-    const promise = axios.get('https://restcountries.eu/rest/v2/all')
-    promise.then(eventHandler)
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      }).catch(error => console.log(error))
   }, [])
 
+  useEffect(() => {
+    console.log("query", query)
+    setFoundCountries(countries.filter(country =>
+      country.name.toLowerCase().includes(query.toLowerCase())))
+  }, [query])
 
 
   return (
+
     <div>
       <div>
-        filter shown with: <input value={query} onChange={handleFilter} />
+        filter shown with: <input value={query} onChange={handleQuery} />
       </div>
       <div>
-        {displayCountry()}
+        {foundCountries.length > 10 &&
+          <p>Too many matches, specify another filter</p>
+        }
+        {
+          foundCountries.length === 1 && <Country country={foundCountries[0]} />
+        }
+        {foundCountries.length > 1 && foundCountries.length < 10
+          && <div>
+            {foundCountries.map(country =>
+              <div key={country.name}> {country.name}
+                <button onClick={() => showSingleCountry(country)}>show</button>
+              </div>
+            )}
+          </div>
+        }
       </div>
     </div>
   )
 }
+
 
 
 export default App
